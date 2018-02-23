389043b6416e3337e98d4506e79d481d739e57e2
Mateusz Pierzchala <mateusz.pierzchala@agidens.com>
2018-02-23 16:05:11 +0100

    v0.2

--- kurs_pluralsight_STL/sorting_vectors.cpp
+++ kurs_pluralsight_STL/sorting_vectors.cpp
@@ -11,29 +11,32 @@ using namespace std;
 
 int main()
 {
-	std::vector<int> v{3,4,5,6,8,23,23,23,45,24,-3,-65,23, 0 , -19}	;
+	void wypisz(vector<int>& v);
+
+	vector<int> v{3,4,5,6,8,23,23,23,45,24,-3,-65,23, 0 , -19}	;
 	auto v2=v;
-	for (std::vector<int>::const_iterator i = v.begin(); i != v.end(); ++i)
-   	 	std::cout << *i << ' ';
-    	cout << endl;
+	wypisz(v);
 	sort(begin(v2), end(v2));
-	for (std::vector<int>::const_iterator i = v2.begin(); i != v2.end(); ++i)
-    	std::cout << *i << ' ';
-    	cout << endl;
+	wypisz(v2);
 
 	sort(begin(v2), end(v2), [](int e1, int e2){return e1>e2;});
-	for (std::vector<int>::const_iterator i = v2.begin(); i != v2.end(); ++i)
-    	std::cout << *i << ' ';
-    	cout << endl;
-
+	wypisz(v2);
 	sort(begin(v2), end(v2), [](int e1, int e2){return abs(e2)>abs(e1);});
-	for (std::vector<int>::const_iterator i = v2.begin(); i != v2.end(); ++i)
-    	std::cout << *i << ' ';
-    	cout << endl;
+	wypisz(v2);
 
 
-auto sorted = is_sorted(begin(v2),end(v2));
-sorted = is_sorted(begin(v2), end(v2), [](int e1, int e2){return abs(e2) > abs(e1);});
-cout << sorted;
-	return 0;
+	auto sorted = is_sorted(begin(v2),end(v2));
+	sorted = is_sorted(begin(v2), end(v2), [](int e1, int e2){return abs(e2) > abs(e1);});
+	cout << sorted;
+	
+
+		return 0;
 }
+
+void wypisz(vector<int>& v)
+	{
+		for (auto i: v)
+    	cout << i << ' ';
+    	cout << endl;
+		
+	}
